a974d9c27c89ee1125cb3b01de5d500a243a3c1e
Osazee Oghagbon <63594399+osazeejedi@users.noreply.github.com>
2022-07-24 16:47:19 +0000

    commit

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your Covalent API key (the one used here is a default for all of our templates)
-    "key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
+    "key": "ckey_7ef6da3d6cad4cf88f3fc480381",
 
     // 2. Set the title of your DEX dashboard
-    "title": "XY=K DEX Dashboard",
+    "title": "DEX-SENSE Dashboard",
 
     // 3. (Optional) Set  your header banner image
     "banner_picture": "",
